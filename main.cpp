47c1568a3c2d4247c87851c88c0cd2aeecc30bd3
Grace Murphy <gracemurphy@Graces-MacBook-Air-3.local>
2024-11-17 11:31:09 -0500

    generate rand nums

--- main.cpp
+++ main.cpp
@@ -54,7 +54,25 @@ int bin_search(vector<int> &keys, int key, int &numCmp) {
 
 // Enhanced binary search: switches to sequential search if remaining range is < 15
 int bin2_search(vector<int> &keys, int key, int &numCmp) {
+    int left = 0, right = keys.size() - 1;
+        numCmp = 0;
 
+        while (left <= right) {
+            if (right - left + 1 < 15) { // Switch to sequential search
+                return seq_search(keys, key, left, right, numCmp);
+            }
+
+            int mid = left + (right - left) / 2;
+            numCmp++;
+            if (keys[mid] == key) {
+                return mid; 
+            } else if (keys[mid] < key) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1; 
 }
 
 // Test the search algorithms
@@ -63,12 +81,25 @@ int main() {
     srand(time(NULL));
 
     // Create a vector of 100,000 integers
+    vector<int> keys(100000);
+    for (int &key : keys) {
+        key = rand() % 10000000;
+    }
  
     // Sort the vector using STL sort
+    sort(keys.begin(), keys.end());
 
     // Generate 50 random numbers from the data (guaranteed to be in the array)
+    vector<int> foundKeys(50);
+    for (int &key : foundKeys) {
+        key = keys[rand() % keys.size()];
+    }
 
     // Generate 50 random numbers (may or may not be in the array)
+    vector<int> randomKeys(50);
+    for (int &key : randomKeys) {
+        key = rand() % 10000000;
+    }
 
     // Test each search algorithm, output results
 
