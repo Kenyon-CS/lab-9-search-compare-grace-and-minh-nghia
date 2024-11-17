29b2fd1a0e08c0b252e6bfe6f33ac0752b97c94d
Grace Murphy <gracemurphy@Graces-MacBook-Air-3.local>
2024-11-17 10:59:08 -0500

    seq search function

--- main.cpp
+++ main.cpp
@@ -8,6 +8,14 @@ using namespace std;
 
 // Sequential search
 int seq_search(vector<int> &keys, int key, int &numCmp) {
+    numCmp = 0;
+    for (size_t i = 0; i < keys.size(); ++i) {
+        numCmp++;
+        if (keys[i] == key) {
+            return i; // returns the index of the key when it is found
+        }
+    }
+    return -1; // returns -1 if key isn't found in the search
   
 }
 
